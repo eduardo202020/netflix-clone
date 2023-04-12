b7a5d7f4bd8dc75dd5a813b66530d88e75929bc2
Eduardo26 <jguevaral@uni.pe>
2023-04-11 20:34:48 -0500

    fix types _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 //@ts-ignore
-export default function App({
+import { FC } from "react";
+
+interface AppProps {
+  Component: FC;
+  pageProps: Record<any, any>;
+}
+
+const App: FC<AppProps> = ({
   Component,
   pageProps: { session, ...pageProps },
-}) {
+}) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-}
+};
+
+export default App;
